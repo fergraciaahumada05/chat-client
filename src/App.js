5a3bd740c48c642adf5c286fc3718afb44af1ae8
fergraciaahumada05 <gracia.fernando1205@gmail.com>
2025-11-02 04:24:49 +0100

    Update server URL to production Render deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import EmojiPicker from "emoji-picker-react";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://chat-server-clfc.onrender.com");
 
 function App() {
   const [message, setMessage] = useState("");
